5526e1d03066b202c05c2a1ab79f32877e05800b
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-03-04 14:54:46 +0100

    My first localhost

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function(request, response) {
+  if (request.method === 'GET' && request.url === '/') {
+    fs.readFile('./index.html', 'utf-8', function(err, data) {
+      response.writeHead(200, {
+        "Content-Type": "text/html; charset=utf-8"
+      });
+      if (!err) response.write(data);
+      response.end();
+    });
+
+  } else {
+    fs.readFile('./Cant_find_what_your_looking_for_ml.jpg', function(err, data) {
+      response.statusCode = 404;
+      if (!err) response.write(data);
+      response.end();
+    });
+  }
+});
+server.listen(8080);
+
+
